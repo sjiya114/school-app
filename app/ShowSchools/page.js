38749e6bcb115adba6f7b629aade7f3935b5d286
sjiya114 <sjiya04072004@gmail.com>
2025-09-04 01:25:58 +0530

    edit

--- app/ShowSchools/page.js
+++ app/ShowSchools/page.js
@@ -1,8 +1,10 @@
 "use client";
 
+
 import axios from "axios";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import Image from "next/image";
 
 export default function page() {
 const [datas,setDatas]=useState([]);
@@ -29,7 +31,7 @@ const [datas,setDatas]=useState([]);
     <div className="flex max-w-screen-xl mx-auto flex-row flex-wrap space-y-10 justify-evenly">
       {datas.map((data,index)=>(
         <div key={index} className="flex  text-white border-gray-500 shadow-white shadow-md hover:scale-105  border-2 rounded-lg flex-col space-y-2 ">
-           <img src={data.image} className="w-66 h-50 rounded-lg" alt="image" />
+           <Image src={data.image} width={264} height={200} priority={index===0}  className="object-contain rounded-lg" alt="image" />
            <div className="flex  flex-col  px-4 py-4 space-y-2">
             <h1 className="text-2xl font-sans leading-tight">{data.name}</h1>
           <p className="text-lg">{data.address}</p>
